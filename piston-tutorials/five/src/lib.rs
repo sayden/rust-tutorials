94fd7aa96bfa23c8dacc395cce26d5464deecb74
Mario Castro <mariocaster@gmail.com>
2016-03-31 23:04:52 +0200

    Structs of ball and box improved

--- piston-tutorials/five/src/lib.rs
+++ piston-tutorials/five/src/lib.rs
@@ -4,7 +4,7 @@ extern crate graphics;
 use piston_window::*;
 
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Clone)]
 enum Direction {
     Left,
     Right,
@@ -30,21 +30,78 @@ impl Ball {
             y_direction: Direction::Down,
         }
     }
+
+    fn draw(&self, c: Context, g: &mut G2d) {
+        ellipse([1.0, 0.0, 0.0, 1.0],
+                [self.x_pos, self.y_pos, self.diameter, self.diameter],
+                c.transform,
+                g);
+    }
+
+    fn check_new_position(&mut self, game: &Game) {
+        let cur = self.x_direction.clone();
+        self.x_direction = match self.x_direction {
+            Direction::Right if (self.x_pos >= game.width - self.diameter) => Direction::Left,
+            Direction::Left if (self.x_pos <= 0f64) => Direction::Right,
+            _ => cur,
+        };
+
+        let cur = self.y_direction.clone();
+        self.y_direction = match self.y_direction {
+            Direction::Down if (self.y_pos >= game.height - self.diameter) => Direction::Up,
+            Direction::Up if (self.y_pos <= 0f64) => Direction::Down,
+            _ => cur,
+        };
+
+        match self.x_direction {
+            Direction::Right => self.x_pos += 0.5f64,
+            Direction::Left => self.x_pos -= 0.5f64,
+            _ => self.x_pos += 0.0f64,
+        }
+
+        match self.y_direction {
+            Direction::Down => self.y_pos += 0.5f64,
+            Direction::Up => self.y_pos -= 0.5f64,
+            _ => self.y_pos -= 0.0f64,
+        }
+    }
 }
 
-pub struct Game_Box {
+pub struct GameBox {
     x_pos: f64,
     y_pos: f64,
     size: f64,
 }
 
-impl Game_Box {
+impl GameBox {
     pub fn new(g: &Game, size: f64) -> Self {
-        Game_Box {
+        GameBox {
             x_pos: (g.width / 2f64) - size / 2f64,
             y_pos: (g.height / 2f64) - size / 2f64,
             size: size,
         }
+    }
+
+    fn draw(&self, c: Context, g: &mut G2d, ball: &Ball) {
+        // It's over if ball position + ball_size is greater than box position and lower than
+        // box position + box_size
+        let over = match (ball.x_pos, ball.y_pos) {
+            (pos_x, pos_y) if (pos_x + ball.diameter) >= self.x_pos &&
+                              pos_x <= (self.x_pos + self.size) &&
+                              (pos_y + ball.diameter) >= self.y_pos &&
+                              pos_y <= (self.y_pos + self.size) => true,
+            _ => false,
+        };
+
+        let color = match over {
+            true => [0.0, 0.0, 1.0, 1.0],
+            false => [1.0, 0.0, 0.0, 1.0],
+        };
+
+        rectangle(color,
+                  [self.x_pos, self.y_pos, self.size, self.size],
+                  c.transform,
+                  g);
     }
 }
 
@@ -73,49 +130,11 @@ pub fn five() {
 
 
     let mut ball = Ball::new(0.0, 0.0, 50.0);
-
-    let game_box = Game_Box::new(&game, 100 as f64);
-
-    // let mut box_x_pos: f64 = (game.width / 2f64) - game_box.size / 2f64;
-    // let mut box_y_pos: f64 = (game.height / 2f64) - game_box.size / 2f64;
-
-    let mut over: bool;
+    let mut game_box = GameBox::new(&game, 100 as f64);
 
     for e in window {
 
-        ball.x_direction = match ball.x_direction {
-            Direction::Right if (ball.x_pos >= game.width - ball.diameter) => Direction::Left,
-            Direction::Left if (ball.x_pos <= 0f64) => Direction::Right,
-            _ => ball.x_direction,
-        };
-
-        ball.y_direction = match ball.y_direction {
-            Direction::Down if (ball.y_pos >= game.height - ball.diameter) => Direction::Up,
-            Direction::Up if (ball.y_pos <= 0f64) => Direction::Down,
-            _ => ball.y_direction,
-        };
-
-        match ball.x_direction {
-            Direction::Right => ball.x_pos += 0.5f64,
-            Direction::Left => ball.x_pos -= 0.5f64,
-            _ => ball.x_pos += 0.0f64,
-        }
-
-        match ball.y_direction {
-            Direction::Down => ball.y_pos += 0.5f64,
-            Direction::Up => ball.y_pos -= 0.5f64,
-            _ => ball.y_pos -= 0.0f64,
-        }
-
-        // It's over if ball position + ball_size is greater than box position and lower than
-        // box position + box_size
-        over = match (ball.x_pos, ball.y_pos) {
-            (pos_x, pos_y) if (pos_x + ball.diameter) >= game_box.x_pos &&
-                              pos_x <= (game_box.x_pos + game_box.size) &&
-                              (pos_y + ball.diameter) >= box_y_pos &&
-                              pos_y <= (box_y_pos + game_box.size) => true,
-            _ => false,
-        };
+        ball.check_new_position(&game);
 
         let key_press: Direction;
 
@@ -125,8 +144,8 @@ pub fn five() {
                 match inp {
                     Input::Press(but) => {
                         match but {
-                            Button::Keyboard(Key::Up) => box_y_pos -= 5f64,
-                            Button::Keyboard(Key::Down) => box_y_pos += 5f64,
+                            Button::Keyboard(Key::Up) => game_box.y_pos -= 5f64,
+                            Button::Keyboard(Key::Down) => game_box.y_pos += 5f64,
                             Button::Keyboard(Key::Right) => game_box.x_pos += 5f64,
                             Button::Keyboard(Key::Left) => game_box.x_pos -= 5f64,
                             _ => game_box.x_pos += 0f64,
@@ -142,20 +161,11 @@ pub fn five() {
         e.draw_2d(|c, g| {
             clear([1.0; 4], g);
 
-            ellipse([1.0, 0.0, 0.0, 1.0],
-                    [ball.x_pos, ball.y_pos, ball.diameter, ball.diameter],
-                    c.transform,
-                    g);
+            ball.draw(c, g);
 
-            let color = match over {
-                true => [0.0, 0.0, 1.0, 1.0],
-                false => [1.0, 0.0, 0.0, 1.0],
-            };
-
-            rectangle(color,
-                      [game_box.x_pos, box_y_pos, game_box.size, game_box.size],
-                      c.transform,
-                      g);
+            // Draw the box, we pass it the ball to check collisions against box and
+            // act accordingly
+            game_box.draw(c, g, &ball)
         });
     }
 }
