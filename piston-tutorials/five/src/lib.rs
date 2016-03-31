0bf49b25f186675e2bf5d65310a9f328ef4d08aa
Mario Castro <mariocaster@gmail.com>
2016-03-31 22:38:31 +0200

    Ball taken to a struct

--- piston-tutorials/five/src/lib.rs
+++ piston-tutorials/five/src/lib.rs
@@ -16,19 +16,33 @@ pub struct Ball {
     x_pos: f64,
     y_pos: f64,
     diameter: f64,
+    x_direction: Direction,
+    y_direction: Direction,
 }
 
-pub struct Box {
+impl Ball {
+    pub fn new(start_x_pos: f64, start_y_pos: f64, diameter: f64) -> Self {
+        Ball {
+            x_pos: start_x_pos,
+            y_pos: start_y_pos,
+            diameter: diameter,
+            x_direction: Direction::Right,
+            y_direction: Direction::Down,
+        }
+    }
+}
+
+pub struct Game_Box {
     x_pos: f64,
     y_pos: f64,
-    size: u32,
+    size: f64,
 }
 
-impl Box {
-    pub fn new(g: &Game, size: u32) -> Self {
-        Box {
-            x_pos: (g.width / 2f64) - size as f64 / 2f64,
-            y_pos: (g.height / 2f64) - size as f64 / 2f64,
+impl Game_Box {
+    pub fn new(g: &Game, size: f64) -> Self {
+        Game_Box {
+            x_pos: (g.width / 2f64) - size / 2f64,
+            y_pos: (g.height / 2f64) - size / 2f64,
             size: size,
         }
     }
@@ -57,54 +71,49 @@ pub fn five() {
                                    .build()
                                    .unwrap();
 
-    let mut ball_x_pos: f64 = 0.0;
-    let mut ball_y_pos: f64 = 0.0;
 
-    let ball_size = 50.0;
+    let mut ball = Ball::new(0.0, 0.0, 50.0);
 
-    let game_box = Box::new(&game, 100);
+    let game_box = Game_Box::new(&game, 100 as f64);
 
-    let mut box_x_pos: f64 = (game.width / 2f64) - game_box.size as f64 / 2f64;
-    let mut box_y_pos: f64 = (game.height / 2f64) - game_box.size as f64 / 2f64;
-
-    let mut ball_x_direction = Direction::Right;
-    let mut ball_y_direction = Direction::Down;
+    let mut box_x_pos: f64 = (game.width / 2f64) - game_box.size / 2f64;
+    let mut box_y_pos: f64 = (game.height / 2f64) - game_box.size / 2f64;
 
     let mut over: bool;
 
     for e in window {
 
-        ball_x_direction = match ball_x_direction {
-            Direction::Right if (ball_x_pos >= game.width - ball_size) => Direction::Left,
-            Direction::Left if (ball_x_pos <= 0f64) => Direction::Right,
-            _ => ball_x_direction,
+        ball.x_direction = match ball.x_direction {
+            Direction::Right if (ball.x_pos >= game.width - ball.diameter) => Direction::Left,
+            Direction::Left if (ball.x_pos <= 0f64) => Direction::Right,
+            _ => ball.x_direction,
         };
 
-        ball_y_direction = match ball_y_direction {
-            Direction::Down if (ball_y_pos >= game.height - ball_size) => Direction::Up,
-            Direction::Up if (ball_y_pos <= 0f64) => Direction::Down,
-            _ => ball_y_direction,
+        ball.y_direction = match ball.y_direction {
+            Direction::Down if (ball.y_pos >= game.height - ball.diameter) => Direction::Up,
+            Direction::Up if (ball.y_pos <= 0f64) => Direction::Down,
+            _ => ball.y_direction,
         };
 
-        match ball_x_direction {
-            Direction::Right => ball_x_pos += 0.5f64,
-            Direction::Left => ball_x_pos -= 0.5f64,
-            _ => ball_x_pos += 0.0f64,
+        match ball.x_direction {
+            Direction::Right => ball.x_pos += 0.5f64,
+            Direction::Left => ball.x_pos -= 0.5f64,
+            _ => ball.x_pos += 0.0f64,
         }
 
-        match ball_y_direction {
-            Direction::Down => ball_y_pos += 0.5f64,
-            Direction::Up => ball_y_pos -= 0.5f64,
-            _ => ball_y_pos -= 0.0f64,
+        match ball.y_direction {
+            Direction::Down => ball.y_pos += 0.5f64,
+            Direction::Up => ball.y_pos -= 0.5f64,
+            _ => ball.y_pos -= 0.0f64,
         }
 
         // It's over if ball position + ball_size is greater than box position and lower than
         // box position + box_size
-        over = match (ball_x_pos, ball_y_pos) {
-            (pos_x, pos_y) if (pos_x + ball_size) >= box_x_pos &&
+        over = match (ball.x_pos, ball.y_pos) {
+            (pos_x, pos_y) if (pos_x + ball.diameter) >= box_x_pos &&
                               pos_x <= (box_x_pos + game_box.size) &&
-                              (pos_y + ball_size) >= box_y_pos &&
-                              pos_y <= (box_y_pos) + game_box.size => true,
+                              (pos_y + ball.diameter) >= box_y_pos &&
+                              pos_y <= (box_y_pos + game_box.size) => true,
             _ => false,
         };
 
@@ -134,7 +143,7 @@ pub fn five() {
             clear([1.0; 4], g);
 
             ellipse([1.0, 0.0, 0.0, 1.0],
-                    [ball_x_pos, ball_y_pos, ball_size, ball_size],
+                    [ball.x_pos, ball.y_pos, ball.diameter, ball.diameter],
                     c.transform,
                     g);
 
